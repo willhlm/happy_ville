44ffc3f95745b80c0417e3c1503b3d71c421e6ed
Ola Kenji Forslund <ola.kenji@gmail.com>
2021-05-29 19:59:36 +0200

    merge

--- Level.py
+++ Level.py
@@ -262,8 +262,8 @@ class Auto(Camera):
         super().__init__()
 
     def scrolling(self,knight,distance,shake):
-        self.true_scroll[0]+=(knight.center[0]-4*self.true_scroll[0]-240)/20
-        self.true_scroll[1]+=(knight.center[1]-self.true_scroll[1]-180)
+        self.true_scroll[0]+=(knight.center[0]-1*self.true_scroll[0]-288)/1
+        self.true_scroll[1]+=(knight.center[1]-self.true_scroll[1]-240)
         self.update_scroll(shake)
 
 class Autocap(Camera):
