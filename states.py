2484966c35742057330f4306db0fac32c3844942
willhlm <william.holmkvist@gmail.com>
2024-06-18 23:55:33 +0200

    6/8 interiors started/finished

--- states.py
+++ states.py
@@ -105,7 +105,7 @@ class Title_Menu(Game_State):
             new_state.enter_state()
 
             #load new game level
-            self.game.game_objects.load_map(self,'village_ola2_15','1')
+            self.game.game_objects.load_map(self,'village_ola2_16','1')
             #self.game.game_objects.load_map(self,'light_forest_cave_1','1')
             #self.game.game_objects.load_map(self,'light_forest_6','1')
 
