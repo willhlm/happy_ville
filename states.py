8abba61309bf4bb318c3f6f66c5e70cc7b19da79
willhlm <william.holmkvist@gmail.com>
2024-07-25 23:43:52 +0200

    aslat mountain semi done

--- states.py
+++ states.py
@@ -108,8 +108,8 @@ class Title_Menu(Game_State):
             new_state.enter_state()
 
             #load new game level
-            #self.game.game_objects.load_map(self,'village_ola2_1','1')
-            self.game.game_objects.load_map(self,'light_forest_17','1')
+            self.game.game_objects.load_map(self,'village_ola2_5','1')
+            #self.game.game_objects.load_map(self,'light_forest_17','1')
 
         elif self.current_button == 1:
             new_state = Load_Menu(self.game)
