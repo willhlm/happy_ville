e4f82f4ef04d862ff857c7684c725eceb21cbe4a
willhlm <william.holmkvist@gmail.com>
2024-08-06 23:32:26 +0200

    thor mtn

--- maps/village_ola2/objects/objects.tsx
+++ maps/village_ola2/objects/objects.tsx
@@ -1,7 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="objects" tilewidth="32" tileheight="64" tilecount="1" columns="0">
+<tileset version="1.10" tiledversion="1.10.1" name="objects" tilewidth="32" tileheight="64" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="64" source="thor_mtn1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="11" height="31" source="../../../Sprites/animations/bg_animations/thor_mtn_village/idle/lightning8.png"/>
  </tile>
 </tileset>
