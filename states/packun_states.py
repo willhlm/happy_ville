11a41f43c5918d94002438bf0c2f2e3ac6bfb3d7
OlaKenji <ola.kenji@gmail.com>
2025-05-18 22:10:47 +0200

    packun

--- states/packun_states.py
+++ states/packun_states.py
@@ -14,6 +14,9 @@ class EnemyStates():
     def increase_phase(self):
         pass
 
+    def handle_input(self, input):
+        pass
+
 class Idle(EnemyStates):
     def __init__(self, entity, **kwarg):
         super().__init__(entity)
@@ -21,11 +24,15 @@ class Idle(EnemyStates):
 
     def update(self):
         super().update()
-        self.check_sight()
+        self.entity.angle_state.check_sight()       
+        self.check_attack()
 
-    def check_sight(self):
-        self.entity.angle_state.check_sight()
-        self.entity.angle_state.check_attack()
+    def check_attack(self):
+        if abs(self.entity.currentstate.player_distance[0]) < self.entity.angle_state.attack_distance[0] and abs(self.entity.currentstate.player_distance[1]) < self.entity.angle_state.attack_distance[1]:
+            if self.entity.flags['attack_able']:
+                self.entity.flags['attack_able'] = False
+                self.entity.game_objects.timer_manager.start_timer(100, self.entity.on_attack_timeout)#adds a timer to timer_manager and sets self.invincible to false after a while
+                self.enter_state('Attack_pre')                      
 
 class Attack_pre(EnemyStates):
     def __init__(self, entity, **kwarg):
@@ -44,15 +51,17 @@ class Attack_main(EnemyStates):
     def increase_phase(self):#called from states, depending on if the player was close when it wanted to explode or not
         self.enter_state('Idle')
 
+class Death(EnemyStates):
+    def __init__(self, entity, **kwarg):
+        super().__init__(entity)
+        self.entity.animation.play('death')
+
+    def increase_phase(self):#called from states, depending on if the player was close when it wanted to explode or not
+        self.entity.dead()
+
 class Angle_sates():
     def __init__(self, entity):
-        self.entity = entity
-
-    def check_attack(self):#called feom currentstate, idle
-        pass
-
-    def enter_state(self, newstate, **kwarg):
-        self.entity.currentstate = getattr(sys.modules[__name__], newstate)(self.entity, **kwarg)#make a class based on the name of the newstate: need to import sys   
+        self.entity = entity      
 
     def check_sight(self):#called from currentstate, idle
         pass
@@ -65,14 +74,7 @@ class Up(Angle_sates):
         super().__init__(entity)
         self.angle = 0
         self.attack_distance = [250, 50]
-    
-    def check_attack(self):
-        if abs(self.player_distance[0]) < self.entity.attack_distance[0] and abs(self.player_distance[1]) < self.entity.attack_distance[1]:
-            if self.entity.flags['attack_able']:
-                self.entity.flags['attack_able'] = False
-                self.entity.game_objects.timer_manager.start_timer(100, self.entity.on_attack_timeout)#adds a timer to timer_manager and sets self.invincible to false after a while
-                self.enter_state('Attack_pre')    
-
+                    
     def check_sight(self):
         if self.entity.currentstate.player_distance[0] > 0 and self.entity.dir[0] == -1 or self.entity.currentstate.player_distance[0] < 0 and self.entity.dir[0] == 1:#player on right and looking at left#player on left and looking right            
             self.entity.dir[0] *= -1        
@@ -84,13 +86,7 @@ class Left(Angle_sates):
     def __init__(self, entity):
         super().__init__(entity)    
         self.angle = -90   
-
-    def check_attack(self):
-        if abs(self.player_distance[0]) < self.entity.attack_distance[0] and abs(self.player_distance[1]) < self.entity.attack_distance[1]:
-            if self.entity.flags['attack_able']:
-                self.entity.flags['attack_able'] = False
-                self.entity.game_objects.timer_manager.start_timer(100, self.entity.on_attack_timeout)#adds a timer to timer_manager and sets self.invincible to false after a while
-                self.enter_state('Attack_pre')    
+        self.attack_distance = [50, 250]
 
     def check_sight(self):
         if self.entity.currentstate.player_distance[1] > 0 and self.entity.dir[0] == 1 or self.entity.currentstate.player_distance[1] < 0 and self.entity.dir[0] == -1:#player on right and looking at left#player on left and looking right            
@@ -103,13 +99,7 @@ class Right(Angle_sates):
     def __init__(self, entity):
         super().__init__(entity)      
         self.angle = 90  
-
-    def check_attack(self):
-        if abs(self.player_distance[0]) < self.entity.attack_distance[0] and abs(self.player_distance[1]) < self.entity.attack_distance[1]:
-            if self.entity.flags['attack_able']:
-                self.entity.flags['attack_able'] = False
-                self.entity.game_objects.timer_manager.start_timer(100, self.entity.on_attack_timeout)#adds a timer to timer_manager and sets self.invincible to false after a while
-                self.enter_state('Attack_pre')   
+        self.attack_distance = [50, 250]
 
     def check_sight(self):
         if self.entity.currentstate.player_distance[1] > 0 and self.entity.dir[0] == -1 or self.entity.currentstate.player_distance[1] < 0 and self.entity.dir[0] == 1:#player on right and looking at left#player on left and looking right            
@@ -122,13 +112,7 @@ class Down(Angle_sates):
     def __init__(self, entity):
         super().__init__(entity)
         self.angle = -180
-
-    def check_attack(self):
-        if abs(self.player_distance[0]) < self.entity.attack_distance[0] and abs(self.player_distance[1]) < self.entity.attack_distance[1]:
-            if self.entity.flags['attack_able']:
-                self.entity.flags['attack_able'] = False
-                self.entity.game_objects.timer_manager.start_timer(100, self.entity.on_attack_timeout)#adds a timer to timer_manager and sets self.invincible to false after a while
-                self.enter_state('Attack_pre')            
+        self.attack_distance = [250, 50]          
     
     def check_sight(self):
         if self.entity.currentstate.player_distance[0] > 0 and self.entity.dir[0] == 1 or self.entity.currentstate.player_distance[0] < 0 and self.entity.dir[0] == -1:#player on right and looking at left#player on left and looking right            
