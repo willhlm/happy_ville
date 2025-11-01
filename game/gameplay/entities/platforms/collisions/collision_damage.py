0cba1ed86b4b777544350060b77f9705be89f4e2
willhlm <william.holmkvist@gmail.com>
2025-11-01 03:58:26 +0100

    pre merge

--- game/gameplay/entities/platforms/collisions/collision_damage.py
+++ game/gameplay/entities/platforms/collisions/collision_damage.py
@@ -3,7 +3,8 @@ from gameplay.entities.platforms.base.platform import Platform
 class CollisionDamage(Platform):#"spikes"
     def __init__(self,pos,size):
         super().__init__(pos,size)
-        self.dmg = 1
+        #self.dmg = 1
+        self.damage = 1
 
     def collide_x(self,entity):
         if entity.velocity[0]>0:#going to the right
@@ -12,7 +13,7 @@ class CollisionDamage(Platform):#"spikes"
         else:#going to the left
             entity.left_collision(self)
             entity.velocity[0] = 10#knock back
-        entity.take_dmg(self.dmg)
+        entity.take_dmg(self)
         entity.update_rect_x()
 
     def collide_y(self,entity):
@@ -22,7 +23,7 @@ class CollisionDamage(Platform):#"spikes"
         else:#going up
             entity.top_collision(self)
             entity.velocity[1] = 10#knock back
-        entity.take_dmg(self.dmg)
+        entity.take_dmg(self)
         entity.update_rect_y()
 
 #texture based
