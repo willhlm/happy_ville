8c8ba6728373a39173e97a0cfe835f86d4655308
freek254 <fredrik.ekman.87@gmail.com>
2025-12-16 11:53:36 +0100

    merge

--- game/assets/maps/tall_trees/objects/objects.tsx
+++ game/assets/maps/tall_trees/objects/objects.tsx
@@ -1,23 +1,23 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="objects" tilewidth="64" tileheight="64" tilecount="3" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="objects" tilewidth="64" tileheight="64" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <transformations hflip="1" vflip="0" rotate="0" preferuntransformed="0"/>
  <tile id="10">
   <properties>
    <property name="direction" value=""/>
   </properties>
-  <image width="64" height="48" source="../../../sprites/entities/enemies/packun/idle/idle1.png"/>
+  <image source="../../../sprites/entities/enemies/common/ground/packun/idle/idle1.png" width="64" height="48"/>
  </tile>
  <tile id="11">
   <properties>
    <property name="ID" value=""/>
   </properties>
-  <image width="64" height="64" source="../../../sprites/entities/platforms/breakable/tall_trees/type2/idle/breakable1.png"/>
+  <image source="../../../sprites/entities/platforms/breakable/tall_trees/type2/idle/breakable1.png" width="64" height="64"/>
  </tile>
  <tile id="12">
   <properties>
    <property name="ID" value=""/>
   </properties>
-  <image width="64" height="64" source="../../../sprites/entities/platforms/breakable/tall_trees/type3/idle/breakable1.png"/>
+  <image source="../../../sprites/entities/platforms/breakable/tall_trees/type3/idle/breakable1.png" width="64" height="64"/>
  </tile>
 </tileset>
