2c0f516c5a5afad061ced3f7e7ae5522ba78505a
willhlm <william.holmkvist@gmail.com>
2026-01-02 19:50:12 +0100

    jopp

--- game/gameplay/states/menus/title_menu.py
+++ game/gameplay/states/menus/title_menu.py
@@ -5,7 +5,7 @@ from .base.base_ui import BaseUI
 class TitleMenu(BaseUI):
     def __init__(self,game):
         super().__init__(game)
-        
+
         self.menu_ui = getattr(ui_loader, 'TitleMenu')(game.game_objects)
         self.play_music()
         self.bg = self.menu_ui.sprites['title'][0]
@@ -20,7 +20,7 @@ class TitleMenu(BaseUI):
         self._update_arrow()
         self._update_button()  # Initialize first button as active
 
-    def update_render(self, dt):        
+    def update_render(self, dt):
         self.menu_ui.buttons[self.current_button].active()# Always call active on the current button (for continuous hover effects)
 
         self.time += dt * 0.01
@@ -28,11 +28,11 @@ class TitleMenu(BaseUI):
             arrow.update(dt)#make them move back and forth
 
     def fade_update(self, dt):#called from fade out: update that should be played when fading: it is needed becayse depending on state, only part of the update loop should be called
-        self.update_render(dt)        
+        self.update_render(dt)
 
     def render(self):
         self.game.screen_manager.screen.clear(0,0,0,0)
-        self.image.clear(0,0,0,0)                      
+        self.image.clear(0,0,0,0)
 
         self.game.game_objects.shaders['title_screen']['time'] = self.time
         self.game.display.render(self.bg2, self.game.screen_manager.screen)
@@ -41,7 +41,7 @@ class TitleMenu(BaseUI):
 
         # Blit buttons
         for b in self.menu_ui.buttons:
-            b.render(self.game.screen_manager.screen)            
+            b.render(self.game.screen_manager.screen)
 
         # Blit arrow
         for arrow in self.menu_ui.arrows:
@@ -60,39 +60,39 @@ class TitleMenu(BaseUI):
             else:# left arrow, align to left edge of button
                 arrow.set_pos((bx - arrow.rect.width - 10, y_pos))  # -10 px padding
         arrow.play_SFX()
-    
+
     def _update_button(self):
         """Handle button state transitions when selection changes"""
-        
+
         # Exit the previous button (if there was one)
         if self.previous_button is not None and self.previous_button != self.current_button:
             self.menu_ui.buttons[self.previous_button].on_exit()
-        
+
         # Enter the new button (if it's different)
         if self.previous_button != self.current_button:
             self.menu_ui.buttons[self.current_button].on_enter()
-                
+
         # Update previous button tracker
         self.previous_button = self.current_button
 
     def handle_events(self, input):
         event = input.output()
         input.processed()
-        
+
         if event[2]['l_stick'][1] < 0 or (event[-1] == 'dpad_up' and event[0]):#up
             self.current_button -= 1
             if self.current_button < 0:
                 self.current_button = len(self.menu_ui.buttons) - 1
             self._update_arrow()
             self._update_button()  # Handle button state change
-            
+
         elif event[2]['l_stick'][1] > 0 or (event[-1] == 'dpad_down' and event[0]):#down
             self.current_button += 1
             if self.current_button >= len(self.menu_ui.buttons):
                 self.current_button = 0
             self._update_arrow()
             self._update_button()  # Handle button state change
-            
+
         elif event[0]:
             if event[-1] in ('return', 'a'):
                 self.menu_ui.buttons[self.current_button].pressed()
@@ -116,13 +116,9 @@ class TitleMenu(BaseUI):
             #self.game.game_objects.load_map(self,'wakeup_forest_1','1')
             #self.game.game_objects.load_map(self,'spirit_world_1','1')
             #self.game.game_objects.load_map(self,'crystal_mines_1','1')
-<<<<<<< HEAD
             #self.game.game_objects.load_map(self,'village_5','1')
-            self.game.game_objects.load_map(self,'nordveden_windtest','1')
-=======
-            self.game.game_objects.load_map(self,'village_1','1')
             #self.game.game_objects.load_map(self,'nordveden_windtest','1')
->>>>>>> 22d2e7f78f8660faaaa14c44fcf8e8f57da59c39
+            self.game.game_objects.load_map(self,'village_1','1')
             #self.game.game_objects.load_map(self,'nordveden_1','1')
             #self.game.game_objects.load_map(self,'tall_trees_1','1')
             #self.game.game_objects.load_map(self,'dark_forest_1','5')
